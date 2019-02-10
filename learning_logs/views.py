e4609660bdd5d7cfac8a148d1a63a78e43356756
jranimo <jamesroselle@gmail.com>
2019-02-09 22:16:30 -0500

    replace get with get_object_or_404

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -24,7 +24,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
    """Show a single topic and all its entries."""
-   topic = Topic.objects.get(id=topic_id)
+   topic = get_object_or_404(Topic, id=topic_id)
    # Make sure the topic belongs to the current user.
    check_topic_owner(request, topic)
    entries = topic.entry_set.order_by('-date_added')
